6d5cc23fee192f222fec46dc7e47b414bb3f6d5a
Nicholas Hubbard <hubbardn95@gmail.com>
2020-02-18 10:45:35 -0500

    Big changes: refactored strings into separate file and changed NetworkTables detection formatting. Smaller changes: * Fixed debugging logic. * Disabled TensorFlow deprecation warnings correctly. * Added notice about irrelevance of deprecation warnings on TensorFlow 2.x. * Added import guards for OpenCV, TensorFlow and NetworkTables. * Inverted model not present condition and added message about correct config. * Added default values to all NetworkTables "get" calls. * Fixed visual debugging to not freeze when no object is detected.

--- mlrun/main.py
+++ mlrun/main.py
@@ -10,8 +10,11 @@ from logging import Logger
 import os
 import sys
 from typing import Dict
+import pkg_resources
 
 import coloredlogs
+
+from mlrun import strings
 
 # Load configuration file
 with open("config.json") as fp:
@@ -38,57 +41,72 @@ pipelineEnabled = True
 ###########################################################################
 # Start logging below, because the logger isn't loaded before this point. #
 ###########################################################################
-l["root"].debug("Starting a new instance of MLRun.")
-if config["debugging"]:
-    l["root"].warning("Debugging is enabled! Performance will be dramatically reduced due to displaying of output.")
+l["root"].debug(strings.mlrun_started)
+if (not config["debugging"]["logs"]) and (not config["debugging"]["show"]):
+    l["root"].info(strings.debugging_disabled)
+elif config["debugging"]["logs"] and (not config["debugging"]["show"]):
+    l["root"].info(strings.debugging_logs_only)
+elif (not config["debugging"]["logs"]) and config["debugging"]["show"]:
+    l["root"].info(strings.debugging_show_only)
+elif config["debugging"]["logs"] and config["debugging"]["show"]:
+    l["root"].info(strings.debugging_full_enabled)
 
 # If configured to do so, hide TensorFlow deprecation messages.
 # Note that this is a private API, and may disappear at any time.
 if not config["tensorflow"]["print_deprecation_messages"]:
-    l["tensorflow"].warning("TensorFlow deprecation warnings have been disabled. If you intend on upgrading "
-                            "TensorFlow later, please enable these in your configuration file. Otherwise, "
-                            "you can ignore this message.")
+    l["tensorflow"].warning(strings.tensorflow_deprecation_disabled)
     from tensorflow.python.util import deprecation
 
     deprecation._PRINT_DEPRECATION_WARNINGS = False
+elif pkg_resources.get_distribution("tensorflow").version.split(".")[0] > 1:
+    l["tensorflow"].info(strings.tensorflow_deprecation_irrelevant)
 else:
-    l["tensorflow"].warning("TensorFlow deprecation warnings have been enabled. These can safely be ignored, "
-                            "or turned off in your configuration file if they don't affect your use case.")
+    l["tensorflow"].info(strings.tensorflow_deprecation_enabled)
 
 # If configured to do so, hide TensorFlow's normal output messages.
 if config["tensorflow"]["minimum_logging_level"] is 0:
-    l["tensorflow"].warning("TensorFlow minimum logging level set to allow information, warnings, non-fatal "
-                            "errors, and fatal errors.")
+    l["tensorflow"].warning(strings.tensorflow_log_level_full)
 elif config["tensorflow"]["minimum_logging_level"] is 1:
-    l["tensorflow"].warning("TensorFlow minimum logging level set to allow warnings, non-fatal errors and fatal "
-                            "errors.")
+    l["tensorflow"].warning(strings.tensorflow_log_level_warnings)
 elif config["tensorflow"]["minimum_logging_level"] is 2:
-    l["tensorflow"].warning("TensorFlow minimum logging level set to allow non-fatal errors and fatal errors.")
+    l["tensorflow"].warning(strings.tensorflow_log_level_errors)
 elif config["tensorflow"]["minimum_logging_level"] is 3:
-    l["tensorflow"].warning("Tensorflow minimum logging level set to allow only fatal errors.")
-os.system(f"export TF_CPP_MIN_LOG_LEVEL=\"{config['tensorflow']['minimum_logging_level']}\"")
+    l["tensorflow"].warning(strings.tensorflow_log_level_fatal)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = config["tensorflow"]["minimum_logging_level"]
 
 # Open the camera for reading.
 # Then check whether the capture was successfully opened.
-l["opencv"].info("Loading OpenCV...")
-import cv2
+l["opencv"].info(strings.opencv_loading)
+try:
+    # Note: PyCharm freaks out about OpenCV, b/c supposedly the references we make don't exist.
+    # Spoiler alert: they do.
+    # noinspection PyUnresolvedReferences
+    import cv2
+    # inspection PyUnresolvedReferences
+except ImportError:
+    l["opencv"].error(strings.opencv_unsuccessful)
+    sys.exit(1)
 
-l["opencv"].info("Successfully loaded OpenCV.")
+l["opencv"].info(strings.opencv_successful)
 cap = cv2.VideoCapture(int(config["camera"]["id"]))
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, config["camera"]["width"])
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, config["camera"]["height"])
 cap.set(cv2.CAP_PROP_FPS, config["camera"]["fps"])
 if not cap.isOpened():
-    l["opencv"].error("OpenCV was unable to open the camera /dev/video{cam}. Please check your "
-                      "configuration.".format(cam=config["camera"]["id"]))
+    l["opencv"].error(strings.opencv_camera_error.format(cam=config["camera"]["id"]))
     sys.exit(1)
 
 # Prepare for publishing via NetworkTables, if enabled in the settings file.
 if config["networktables"]["enabled"]:
-    from _pynetworktables import NetworkTable
-    from networktables import NetworkTables
+    l["networktables"].info(strings.networktables_loading)
+    try:
+        from _pynetworktables import NetworkTable
+        from networktables import NetworkTables
+    except ImportError:
+        l["networktables"].error(strings.networktables_unexpected_disable)
+        sys.exit(1)
 
-    l["networktables"].info("NetworkTables support is enabled.")
+    l["networktables"].info(strings.networktables_successful)
 
 
     def connection_listener(status, connection):
@@ -99,57 +117,66 @@ if config["networktables"]["enabled"]:
             connection: ConnectionInfo NamedTuple from NetworkTables
         """
         if status:
-            l["networktables"].info(
-                f"Established connection with {connection.remote_ip}:{connection.remote_port} "
-                f"over protocol version {connection.protocol_version}.")
+            l["networktables"].info(strings.networktables_connection_established.format(
+                ip=connection.remote_ip,
+                port=connection.remote_port,
+                ver=connection.protocol_version
+            ))
         else:
-            l["networktables"].warning(
-                f"Lost connection with NetworkTables server."
-            )
+            l["networktables"].warning(strings.networktables_connection_lost)
+
 
     NetworkTables.addConnectionListener(connection_listener, True)
     NetworkTables.initialize(server="roborio-{team}-frc.local".format(team=config["networktables"]["team"]))
     sd: NetworkTable = NetworkTables.getTable(config["networktables"]["table"])
 else:
-    l["networktables"].warning("NetworkTables support disabled. Results will not be broadcasted over the network.")
+    l["networktables"].warning(strings.networktables_expected_disable)
 
 # Load TensorFlow from disk.
 if config["tensorflow"]["compat"]:
-    l["tensorflow"].warning("TensorFlow is running in V1 (compatibility) mode. This is a kludge for replacing legacy "
-                            "TensorFlow code with modern TensorFlow 2.0 code. Please fix your code later on.")
-    l["tensorflow"].info("Loading TensorFlow in compatibility mode...")
-    import tensorflow.compat.v1 as tf
+    l["tensorflow"].warning(strings.tensorflow_legacy_mode)
+    l["tensorflow"].info(strings.tensorflow_legacy_loading)
+    try:
+        import tensorflow.compat.v1 as tf
+    except ImportError:
+        l["tensorflow"].error(strings.tensorflow_legacy_error)
+        sys.exit(1)
 
-    l["tensorflow"].info("Successfully loaded TensorFlow in compatibility mode.")
+    l["tensorflow"].info(strings.tensorflow_legacy_loaded)
 else:
-    l["tensorflow"].info("TensorFlow is running in V2 (modern) mode.")
-    l["tensorflow"].info("Loading TensorFlow...")
-    import tensorflow as tf
+    l["tensorflow"].info(strings.tensorflow_modern_mode)
+    l["tensorflow"].info(strings.tensorflow_modern_loading)
+    try:
+        import tensorflow as tf
+    except ImportError:
+        l["tensorflow"].error(strings.tensorflow_modern_error)
+        sys.exit(1)
 
-    l["tensorflow"].info("Successfully loaded TensorFlow.")
+    l["tensorflow"].info(strings.tensorflow_modern_loaded)
 
 # Check if a model exists at the specified path.
-if not os.path.exists(config["tensorflow"]["model_path"] + "/saved_model.pb"):
-    l["tensorflow"].error("A model was not found in the specified model path. Please check your configuration and try "
-                          "again.")
+if os.path.exists(config["tensorflow"]["model_path"] + "/saved_model.pb"):
+    l["tensorflow"].info(strings.tensorflow_model_present)
+else:
+    l["tensorflow"].error(strings.tensorflow_model_missing)
     sys.exit(1)
 
 # Notify of frame rate tick frequency.
-fps: float = 1.0
+fps: float = 0.0
 freq: float = cv2.getTickFrequency()
-l["opencv"].info(f"System's stated tick frequency is {freq}.")
+l["opencv"].info(strings.opencv_tick_frequency.format(freq=freq))
 
 try:
-    l["tensorflow"].info("Please wait for TensorFlow to load the model into memory. This may take a while...")
+    l["tensorflow"].info(strings.tensorflow_loading_model)
     with tf.Session(graph=tf.Graph()) as sess:
         tf.saved_model.loader.load(sess, ["serve"], config["tensorflow"]["model_path"])
-        l["root"].info("Startup has finished; inference will begin momentarily.")
+        l["root"].info(strings.tensorflow_loaded_model)
         # Begin process of inference.
         if config["networktables"]["enabled"]:
             sd.putBoolean(f"{config['networktables']['keyPrefix']}/enabled", pipelineEnabled)
         while True:
             if config["networktables"]["enabled"]:
-                if sd.getBoolean(f"{config['networktables']['keyPrefix']}/enabled"):
+                if sd.getBoolean(f"{config['networktables']['keyPrefix']}/enabled", False):
                     continue
             t1 = cv2.getTickCount()
             frame = cap.read()[1]
@@ -158,6 +185,7 @@ try:
             })
             scores = inferred[0].tolist()[0]
             boxes = inferred[1].tolist()[0]
+            detections = []
             for i in range(len(scores)):
                 if (scores[i] > 0.8) and (scores[i] <= 1.0):
                     ymin = int(max(1, (boxes[i][0] * frame.shape[0])))
@@ -167,33 +195,31 @@ try:
                     t2 = cv2.getTickCount()
                     pre_calc = (t2 - t1) / freq
                     fps = 1 / pre_calc
-                    if config["networktables"]["enabled"]:
-                        [
-                            sd.putNumber(f"{config['networktables']['keyPrefix']}{i}/{key}", value)
-                            for key, value in {
-                                "xmin": xmin,
-                                "xmax": xmax,
-                                "ymin": ymin,
-                                "ymax": ymax,
-                                "scores": round(100 * scores[i], 1),
-                                "fps": round(fps, 1)
-                            }
-                        ]
-                    if config["debugging"]["logs"]:
-                        l["root"].debug(f"FPS: {round(fps, 1)}; top left: ({xmin}, {ymin}); bottom right: "
-                                        f"({xmin}, {ymin})")
-                    if config["debugging"]["show"]:
-                        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
-                        cv2.imshow("DEBUG frame", frame)
-                        cv2.waitKey(1) & 0xFF
+                    detections.append([xmin, ymin, xmax, ymax, round(100 * scores[i], 1)])
+            if config["debugging"]["logs"]:
+                for i in detections:
+                    l["root"].debug(strings.debug_log.format(fps=round(fps, 1), xmin=i[1], ymin=i[2], xmax=i[3],
+                                                             ymax=i[4]))
+            if config["debugging"]["show"]:
+                for i in detections:
+                    cv2.rectangle(frame, (i[1], i[2]), (i[3], i[4]), (255, 0, 0), 2)
+                cv2.imshow("MLRun Debugging View", frame)
+                cv2.waitKey(1) & 0xFF
+            if config["networktables"]["enabled"]:
+                sd.putString("jetson/detections", json.dumps({
+                    "fps": round(fps, 1),
+                    "numDetections": len(detections),
+                    "detections": detections
+                }))
 
-        l["root"].info("Pipeline stopped without errors.")
         if config["networktables"]["enabled"]:
+            l["root"].info(strings.stopped_nt)
             sd.putBoolean(f"{config['networktables']['keyPrefix']}/enabled", False)
 except KeyboardInterrupt:
-    l["root"].warning("Pipeline stopped manually.")
+    l["root"].warning(strings.stopped_keyboard)
     if config["networktables"]["enabled"]:
         sd.putBoolean(f"{config['networktables']['keyPrefix']}/enabled", False)
     cap.release()
     if config["debugging"]["show"]:
         cv2.destroyAllWindows()
+    sys.exit(0)
---
+++ mlrun/strings.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""Strings for AutoML.
+
+These strings are used by MLRun to ensure that all of the text shown is correct.
+"""
+mlrun_started: str = "Welcome to MLRun! Starting up..."
+
+debugging_disabled: str = "Debugging is not enabled."
+debugging_logs_only: str = "Log-based debugging is enabled."
+debugging_show_only: str = "Visualization debugging is enabled. Performance will be dramatically reduced."
+debugging_full_enabled: str = "Both log and visualization debugging are enabled. Performance will be dramatically " \
+                              "reduced."
+
+tensorflow_deprecation_disabled: str = "TensorFlow deprecation warnings have been disabled. If you intend on " \
+                                       "upgrading TensorFlow later, please enable these in your configuration file. " \
+                                       "Otherwise, you can ignore this message."
+tensorflow_deprecation_enabled: str = "TensorFlow deprecation warnings have been enabled. These can safely be " \
+                                      "ignored, or turned off in your configuration file if they don't affect your " \
+                                      "use case. "
+tensorflow_deprecation_irrelevant: str = "TensorFlow deprecation warnings are irrelevant for your usage case; you are " \
+                                         "running a more recent version of TensorFlow. "
+
+tensorflow_log_level_full: str = "TensorFlow minimum logging level set to allow information, warnings, non-fatal " \
+                                 "errors, and fatal errors. This may reduce performance; recommended setting is FATAL."
+tensorflow_log_level_warnings: str = "TensorFlow minimum logging level set to allow warnings, non-fatal errors and " \
+                                     "fatal errors. "
+tensorflow_log_level_errors: str = "TensorFlow minimum logging level set to allow non-fatal errors and fatal errors."
+tensorflow_log_level_fatal: str = "Tensorflow minimum logging level set to allow only fatal errors."
+
+opencv_loading: str = "Loading OpenCV..."
+opencv_successful: str = "Successfully loaded OpenCV."
+opencv_unsuccessful: str = "Unable to load OpenCV. Make sure that you have installed OpenCV on your path."
+opencv_camera_error: str = "OpenCV was unable to open the camera /dev/video{cam}. Please check your configuration."
+
+networktables_loading: str = "Loading NetworkTables..."
+networktables_successful: str = "NetworkTables support is enabled."
+networktables_expected_disable: str = "NetworkTables support is disabled. Results will not be broadcasted over the " \
+                                      "network."
+networktables_unexpected_disable: str = "NetworkTables support is enabled, but PyNetworkTables could not be found in " \
+                                        "your Python path. Please make sure that PyNetworkTables is installed."
+networktables_connection_established: str = "Established connection with {ip}:{port} over protocol version {ver}."
+networktables_connection_lost: str = "Lost connection with NetworkTables server."
+
+tensorflow_legacy_mode: str = "TensorFlow is running in legacy mode. This is a kludge for replacing legacy code with " \
+                              "modern TensorFlow 2.0 code. Fix your code, or if your use case is not covered, " \
+                              "you may ignore this warning."
+tensorflow_legacy_loading: str = "Loading TensorFlow in legacy mode..."
+tensorflow_legacy_loaded: str = "TensorFlow loaded in legacy mode."
+tensorflow_legacy_error: str = "TensorFlow could not be loaded in legacy mode. Please make sure that TensorFlow is " \
+                               "installed."
+tensorflow_modern_mode: str = "TensorFlow is running in modern mode."
+tensorflow_modern_loading: str = "Loading TensorFlow in modern mode..."
+tensorflow_modern_loaded: str = "TensorFlow loaded in modern mode."
+tensorflow_modern_error: str = "TensorFlow could not be loaded in modern mode. Please make sure that TensorFlow is " \
+                               "installed."
+
+tensorflow_model_present: str = "A model was located at the location specified in your configuration file; continuing."
+tensorflow_model_missing: str = "A model was not found in the specified location. Please ensure that your model has " \
+                                "a name of 'saved_model.pb' in the specified location."
+
+opencv_tick_frequency: str = "System's stated tick frequency is {freq}."
+
+tensorflow_loading_model: str = "Please wait for TensorFlow to load the model into memory. This may take a long " \
+                                "period of time depending on which platform you are using."
+tensorflow_loaded_model: str = "Model has finished loading; inference will begin momentarily."
+
+debug_log: str = "FPS: {fps}; top left: ({xmin}, {ymin}); bottom right: ({xmin}, {ymin})"
+
+stopped_nt: str = "Pipeline stopped by NetworkTables disable command."
+stopped_keyboard: str = "Pipeline stopped with KeyboardInterrupt."
